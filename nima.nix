8aa7433a5db24b2ab7486308da1c2f7fd89db7bb
Lin Jian <me@linj.tech>
2026-05-24 15:57:43 +0800

    pkg(kkp): init

--- nima.nix
+++ nima.nix
@@ -1,6 +1,27 @@
 emacs:
 
+{ lib, ... }:
+
 {
   package = emacs;
   pedantic = true;
+
+  overlay =
+    final: prev:
+    let
+      wonimaPackages = {
+        kitty-keyboard-protocol = mkEpkg { pname = "kitty-keyboard-protocol"; };
+      };
+      mkEpkg =
+        {
+          pname,
+          packageRequires ? [ ],
+        }:
+        final.melpaBuild {
+          inherit pname packageRequires;
+          version = "0.1.0"; # a dummy version
+          src = lib.path.append ./packages "${pname}.el";
+        };
+    in
+    wonimaPackages // { inherit wonimaPackages; };
 }
